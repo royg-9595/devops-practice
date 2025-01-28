32feeba1de19e6bb305ff89aa80cf81d2d697a1a
Jayanth Roy G <103624355+royg-9595@users.noreply.github.com>
2025-01-28 14:38:02 +0530

    Shell scripting practice

---
+++ Shell Scripting/input1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Author: Jayanth Roy
+#Purpose: Practice Shell Scripting input / reading input
+#Usage: ./ input1.sh
+
+
+echo "Enter a value:"
+
+echo " The val I got: $1"
---
+++ Shell Scripting/variable1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Author: Jayanth Roy
+#Purpose: Practice Devops
+#Usage: ./variable.sh
+
+var1=10
+var2="Tuesday"
+
+echo "Hello $var1 and $var2 !"
+
