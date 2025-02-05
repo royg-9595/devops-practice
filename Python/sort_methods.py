a975441d4b54dcc3a99eb892f751ec193f891910
royg-9595 <jayanthroyg9595@gmail.com>
2025-02-05 18:00:41 +0530

    python practice

---
+++ Python/sort_methods.py
@@ -0,0 +1,15 @@
+# def sort_method(args):
+#     return sorted(args)
+
+# fruits = ["zhello", "jay", "roy"]
+
+# print(sort_method(fruits))
+
+
+# def nth_largest(arr, k):
+#     for i in arr:
+
+sample_tuple=("monday", "tuesday", "wednesday")
+print(type(sample_tuple))
+print(sample_tuple)
+print(sample_tuple[1:2])
